68b928ca12066a0a249cb632598b4ab09f51af70
Thomas <thomas.whyyou@gmail.com>
2015-08-14 15:43:23 -0400

    Changes.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
+echo ""
+
 ORIGINDIR="$HOME/dotfiles"
 if [ ! -d "$ORIGINDIR" ]; then
     echo "~/dotfiles doesn't exist.. :("
     return 1
 fi
-DOTFILES=$(find ~/dotfiles -maxdepth 1 -type f -not \( -name ".gitmodules" \) -name ".*")
 
 # Make sure we have git submodules cloned too.
 if hash git 2>/dev/null; then
-    git --work-tree=$ORIGINDIR --git-dir=$ORIGINDIR/.git submodule update --init --recursive
+    git -C $ORIGINDIR submodule update --init --recursive
 else
     echo "You need to install git first. :("
     return 1
 fi
 
 # Relevant dotfiles
-echo ""
+DOTFILES=$(find ~/dotfiles -maxdepth 1 -type f -not \( -name ".gitmodules" \) -name ".*")
 for file in $DOTFILES; do
     echo "Creating symlink to '$file' in home directory."
     ln -sfn $file ~/$(basename $file)
