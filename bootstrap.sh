1637170e4751807a8c76e2c363cef97a69899ccc
Thomas <thomas.whyyou@gmail.com>
2015-08-14 15:32:16 -0400

    more tweaks.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,15 @@ if [ ! -d "$ORIGINDIR" ]; then
     echo "~/dotfiles doesn't exist.. :("
     return 1
 fi
-
 DOTFILES=$(find ~/dotfiles -maxdepth 1 -type f -not \( -name ".gitmodules" \) -name ".*")
+
+# Make sure we have git submodules cloned too.
+if hash git 2>/dev/null; then
+    git --work-tree=$ORIGINDIR --git-dir=$ORIGINDIR/.git submodule update --init --recursive
+else
+    echo "You need to install git first. :("
+    return 1
+fi
 
 # Relevant dotfiles
 echo ""
