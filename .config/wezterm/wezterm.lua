3f1b6a9829af27c9ca3d5828229652867c8d7938
thomaswhyyou <thomas.whyyou@gmail.com>
2021-06-26 14:55:56 -0400

    More updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,4 +3,21 @@ local wezterm = require 'wezterm';
 return {
   tab_bar_at_bottom = true,
   color_scheme = "Molokai",
+
+  -- Like tmux..
+  leader = { key="a", mods="CTRL", timeout_milliseconds=500 },
+
+  keys = {
+    -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+    -- { key="a", mods="LEADER|CTRL", action=wezterm.action{SendString="\x01"} },
+
+    -- Split panes
+    -- { key="|", mods="LEADER|SHIFT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}} },
+    -- { key="-", mods="LEADER", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}} },
+
+    -- { key="h", mods="CTRL", action=wezterm.action{ActivatePaneDirection="Left"} },
+    -- { key="j", mods="CTRL", action=wezterm.action{ActivatePaneDirection="Down"} },
+    -- { key="k", mods="CTRL", action=wezterm.action{ActivatePaneDirection="Up"} },
+    -- { key="l", mods="CTRL", action=wezterm.action{ActivatePaneDirection="Right"} },
+  }
 }
