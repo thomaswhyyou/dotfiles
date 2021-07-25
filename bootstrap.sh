1c0e93c94d31f65d1fab889cde3d1c0af2cafd08
Thomas <thomas.whyyou@gmail.com>
2021-07-24 23:33:31 -0400

    Some cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,10 +18,6 @@ echo "Creating a symlink to nvim/init.vim in .config directory."
 mkdir -p ~/.config/nvim/
 ln -sfn ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 
-echo "Creating a symlink to wezterm/wezterm.lua in .config directory."
-mkdir -p ~/.config/wezterm/
-ln -sfn ~/dotfiles/.config/wezterm/wezterm.lua ~/.config/wezterm/wezterm.lua
-
 echo "Creating a symlink to alacritty/alacritty.yml in .config directory."
 mkdir -p ~/.config/alacritty/
 ln -sfn ~/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
