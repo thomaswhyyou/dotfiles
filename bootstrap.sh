cf796adf15f6edce75026a477f640c0c6fc8766c
Thomas <thomas.whyyou@gmail.com>
2025-01-03 20:13:50 -0500

    add ghostty config and update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Symlink various config files.
 
 DOTFILES="
@@ -8,18 +7,21 @@ DOTFILES="
 .gitconfig
 .psqlrc
 "
-
 for file in $DOTFILES; do
   echo "Creating a symlink to $file in home directory."
   ln -sfn "./dotfiles/$file" ~/$(basename $file)
 done
 
-echo "Creating a symlink to nvim/init.vim in .config directory."
+echo "Creating a symlink to nvim/init.vim in ~/.config"
 mkdir -p ~/.config/nvim/
 ln -sfn ~/dotfiles/.config/nvim/* ~/.config/nvim/
 
-echo "Creating a symlink to alacritty/alacritty.yml in .config directory."
+echo "Creating a symlink to alacritty/alacritty.yml in ~/.config"
 mkdir -p ~/.config/alacritty/
 ln -sfn ~/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+echo "Creating a symlink to ghostty/config in ~/.config"
+mkdir -p ~/.config/ghostty/
+ln -sfn ~/dotfiles/.config/ghostty/config ~/.config/ghostty/config
 
 echo ""; echo "All done!"
