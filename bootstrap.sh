7a678d150dc67cb84b46fa3b126579b1e1addc9c
Thomas <thomas.whyyou@gmail.com>
2025-02-01 12:07:53 -0500

    bootstrap script updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,17 +12,11 @@ for file in $DOTFILES; do
   ln -sfn "./dotfiles/$file" ~/$(basename $file)
 done
 
-echo "Creating a symlink to nvim/init.vim in ~/.config"
-mkdir -p ~/.config/nvim/
-ln -sfn ~/dotfiles/.config/nvim/* ~/.config/nvim/
+echo "Creating a symlink at ~/.config/ghostty"
+ln -sfn ~/dotfiles/.config/ghostty ~/.config/ghostty
 
-echo "Creating a symlink to alacritty/alacritty.yml in ~/.config"
-mkdir -p ~/.config/alacritty/
-ln -sfn ~/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-
-echo "Creating a symlink to ghostty/config in ~/.config"
-mkdir -p ~/.config/ghostty/
-ln -sfn ~/dotfiles/.config/ghostty/config ~/.config/ghostty/config
+echo "Creating a symlink at ~/.config/nvim"
+ln -sfn ~/dotfiles/.config/nvim ~/.config/nvim
 
 ZSHRC="$HOME/.zshrc"
 echo "Setting up $ZSHRC"
